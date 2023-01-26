5c567a9a2d298b553197bf0b57205a14ed53b15b
Leandrops2022 <leandropereirasoares1987@gmail.com>
2023-01-26 10:45:06 -0300

    Correção de bug na barra de navegação
    realizei mudanças no css e no script postRender, pois ele não estava fazendo a query do tamanho da tela, e em telas acima de 480px causava um bug que quebrava o menu da página

--- scripts/postRender.js
+++ scripts/postRender.js
@@ -1,5 +1,9 @@
+const tela = window.matchMedia("(max-width: 480px)");
+
 document.body.onscroll = function escondeMenu() {
-    ativo = false;
-    menu.style.height = "0vh";
-}
+    if (tela.matches) {
+        ativo = false;
+        menu.style.height = "0vh";
+    }
+}        
 
