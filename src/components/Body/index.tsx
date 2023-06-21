eb1de8dc75831d2401c13ac45d6d08b9def99669
Leandrops2022 <leandropereirasoares1987@gmail.com>
2023-06-21 20:16:11 -0300

    added background video

--- src/components/Body/index.tsx
+++ src/components/Body/index.tsx
@@ -1,9 +1,9 @@
-import styles from './Body.module.scss';
+
 import { Outlet } from 'react-router-dom';
 
 export default function Body() {
     return (
-        <div className={styles.main}>
+        <div>
             <Outlet />
         </div>
     );
